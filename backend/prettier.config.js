4ac221f52e90eaa6363fdb8d19cb4e9be0d5d1ab
Miguel <Miguel Augusto>
2021-06-04 20:26:22 -0300

    adiciona linters

---
+++ backend/prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  tabWidth: 2,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  printWidth: 125,
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.use('/', (request, response) => response.json({ message: 'ping' }));
+
 app.listen(3333, () => {
   console.log('Server running at port 3333');
 });
